c95b8282c04bdd3893874fc0b28931423561de41
marcocastignoli <marco.castignoli@gmail.com>
2018-03-09 11:51:27 +0100

    Added basic function in the model

--- Packages/App/Model.php
+++ Packages/App/Model.php
@@ -26,4 +26,30 @@ abstract class Model
             return $this->Database->query($query, $values);
         }
     }
+
+    public function update($values, $where)
+    {
+        if (is_array($values) && count($values) > 0) {
+            $setFields = "";
+            foreach($values as $chiave => $valore){
+                $setFields .= $chiave . " = :" . $chiave . ",";
+            }
+            $setFields = rtrim($setFields, ",");
+            if(array_key_exists('where', $where) && array_key_exists('arguments', $where) && is_array($where['arguments']) ) {
+                $values = array_merge($values, $where['arguments']);
+            }
+            $query = 'UPDATE ' . static::table . ' SET ' . $setFields . ' WHERE ' . $where['where'];
+            return $this->Database->query($query, $values);
+        }
+    }
+
+    public function delete($where)
+    {
+        $values = [];
+        if(array_key_exists('where', $where) && array_key_exists('arguments', $where) && is_array($where['arguments']) ) {
+            $values = $where['arguments'];
+        }
+        $query = 'DELETE FROM ' . static::table . ' WHERE ' . $where['where'];
+        return $this->Database->query($query, $values);
+    }
 }
